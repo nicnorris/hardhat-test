732bc99d655919c0f08870f8e91380819f18339f
nicnorris <nicnorrismail@gmail.com>
2021-08-03 11:02:27 -0700

    2nd commit

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,16 +3,12 @@ const { assert } = require("chai");
 describe("Greeter", function () {
   it("should return sum of x and y", async function () {
     const NewContract = await ethers.getContractFactory("NewContract");
-    const newContract = await Greeter.deploy(x + y);
+    const newContract = await NewContract.deploy();
     await newContract.deployed();
 
-    assert(await greeter.greet()).to.equal(7);
+    const sum = await newContract.sum()
+    assert.equal(sum, 7)
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    
   });
 });
